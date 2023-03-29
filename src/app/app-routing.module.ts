cd12ef9e223ff8e5d70213a77e6d406c3fec0a40
Manoj Singh Rawat <manojrawat1502@gmail.com>
2023-03-29 16:52:47 +0530

    chore: added guard on uci page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'uci-admin',
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     component: MainComponent,
     children: [
       {
