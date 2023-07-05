0a743d1c171428f0e686151fec76518b4aa3d02e
prtkjakhar <prtkjakhar@gmail.com>
2023-07-05 16:23:55 +0530

    added userid and pass in env

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -4,13 +4,17 @@ describe('Login page', () => {
   });
 
   it('should have user id input box with correct attributes', () => {
-    cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(1) > input')
+    cy.get(
+      '#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(1) > input'
+    )
       .should('exist')
       .should('have.attr', 'name', 'loginId');
   });
 
   it('should have password input box with correct attributes', () => {
-    cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(2) > input')
+    cy.get(
+      '#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(2) > input'
+    )
       .should('exist')
       .should('have.attr', 'name', 'password');
   });
@@ -19,5 +23,26 @@ describe('Login page', () => {
     cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > button')
       .should('exist')
       .should('have.text', 'Sign in');
+  });
+
+  it('should sign in and check if the user is logged in', () => {
+    // Enter values in the user id and password inputs
+    cy.get(
+      '#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(1) > input'
+    ).type(Cypress.env('userid'));
+    cy.get(
+      '#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(2) > input'
+    ).type(Cypress.env('pass'));
+
+    // Press the sign-in button
+    cy.get(
+      '#root > div > div.row > div > div > div.col-4.p-5 > form > button'
+    ).click();
+
+    cy.wait(2000);
+    cy.get(
+      '#root > div > div.row > div.col-2.p-0 > div > aside > div > div > div.css-1vmkajq > div > p'
+    ).should('exist'); // Assuming the UCI Dashboard text appears after logging in
+    // Add more assertions to verify the presence of logged-in elements or updated application state
   });
 });
