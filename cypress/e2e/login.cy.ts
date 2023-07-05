fe785bd4452a6b5e22eb7bcaf8b4b0f94302566b
prtkjakhar <prtkjakhar@gmail.com>
2023-07-05 14:42:44 +0530

    updated login page test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should have user id input box with correct attributes', () => {
+    cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(1) > input')
+      .should('exist')
+      .should('have.attr', 'name', 'loginId');
+  });
+
+  it('should have password input box with correct attributes', () => {
+    cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > div:nth-child(2) > input')
+      .should('exist')
+      .should('have.attr', 'name', 'password');
+  });
+
+  it('should have a sign in button with correct attributes', () => {
+    cy.get('#root > div > div.row > div > div > div.col-4.p-5 > form > button')
+      .should('exist')
+      .should('have.text', 'Sign in');
+  });
+});
