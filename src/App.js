86784b7bd758c38db4d9f59dc98eaafce2793309
psankhe28 <sankhepratiksha3@gmail.com>
2023-07-22 17:28:42 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import { Navigate, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import SidebarComponent from "./components/sidebar";
+import { SidebarComponent } from "./components/sidebar";
 import { MDBCol, MDBRow } from "mdb-react-ui-kit";
-import { useContext, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AppContext } from "./provider/contextProvider";
 import { useAuthContext } from "./provider/authProvider";
 import RequireAuth from "./hoc/requireAuth";
 import Loader from "./components/fullscreenLoader";
 import { useStore } from "./store";
-import { SuccessScreen, Dashboard, Login, Add } from "./pages";
+import { SuccessScreen, Dashboard, Login, Add, Overview } from "./pages";
 import { history } from "./utils/history";
 import { useAuth } from "./hooks/useAuth";
 import { AuthContext } from "./provider/authProvider";
+import useWindowSize from "./hooks/useWindow";
+import Kafka from "./pages/monitoring/kafka";
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const values = useMemo(() => ({ isLoading, setIsLoading }), [isLoading]);
- 
+  const [collapsed, setCollapsed] = useState(false);
+  const { width } = useWindowSize();
+
+  const handleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  useEffect(() => {
+    if (width <= 768) {
+      setCollapsed(true);
+    } else {
+      setCollapsed(false);
+    }
+  }, [width]);
 
   const store = useStore();
   const location = useLocation();
@@ -26,27 +48,30 @@ function App() {
 
   history.navigate = useNavigate();
   history.location = useLocation();
-  const theme=useMemo(()=>{
-    if(store?.theme === 'dark') 
-    return {
-      background:'#1b1d21',
-      color:'white'
-    }
-    return {background:'#f3f6f9'}
-    
-  },[store?.theme]);
-  const user=useMemo(()=>store.user,[store.user]);
- 
+  const theme = useMemo(() => {
+    if (store?.theme === "dark")
+      return {
+        background: "#1b1d21",
+        color: "white",
+      };
+    return { background: "#f3f6f9" };
+  }, [store?.theme]);
+  const user = useMemo(() => store.user, [store.user]);
+
   return (
-    <div style={{height:'100vh' ,overflow:'scroll',...theme}}>
+    <div style={{ height: "100vh", overflow: "hidden", ...theme }}>
       <AppContext.Provider value={values}>
         <>
           <Loader loading={showLoader} />
           <MDBRow>
-           {user && <MDBCol size={2} className="p-0">
-              <SidebarComponent />
-            </MDBCol>}
-            <MDBCol size={user ? 10 : 12}>
+            <user && MDBCol size={collapsed ? 1 : 2} className="p-0">
+              {" "}
+              <SidebarComponent
+                collapsed={collapsed}
+                handleCollapse={handleCollapse}
+              />
+            </MDBCol>
+            <MDBCol size={user && collapsed ? 11 : 10}>
               <Routes>
                 {user ? (
                   <Route path="/login" element={<Navigate to={pathName} />} />
@@ -57,7 +82,7 @@ function App() {
                   path="/"
                   element={
                     <RequireAuth>
-                      <Dashboard />
+                    <Dashboard />
                     </RequireAuth>
                   }
                 />
@@ -65,8 +90,8 @@ function App() {
                   path="/add-bot"
                   element={
                     <RequireAuth>
-                      <Add />
-                    </RequireAuth>
+                    <Add />
+                 </RequireAuth>
                   }
                 />
                 <Route
@@ -77,6 +102,22 @@ function App() {
                     </RequireAuth>
                   }
                 />
+                <Route
+                  path="/monitoring"
+                  element={
+                    <RequireAuth>
+                      <Overview theme={theme} />
+                    </RequireAuth>
+                  }
+                />
+                <Route
+                  path="/monitoring/overview"
+                  element={
+                    <RequireAuth>
+                      <Overview theme={theme} />
+                    </RequireAuth>
+                  }
+                />  
               </Routes>
             </MDBCol>
           </MDBRow>
@@ -105,6 +146,7 @@ function App() {
           />
         </>
       </AppContext.Provider>
+      {/* <SidebarComponent /> */}
     </div>
   );
 }
