aa1c43aa8016822c8e8dcc17d2b30523fc2d7324
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2023-09-04 21:33:35 +0530

    Update Menu.tsx

--- src/components/sidebar/Menu.tsx
+++ src/components/sidebar/Menu.tsx
@@ -4,6 +4,10 @@ export const monitoringItemsConfig = [
     link: "/monitoring/overview",
   },
   {
+    label: "Kafka Topics",
+    link: "/monitoring/kafka-topics",
+  },
+  {
     label: "UCI-API",
     link: "/monitoring/uci-api",
   },
@@ -26,10 +30,18 @@ export const monitoringItemsConfig = [
   {
     label: "Outbound",
     link: "/monitoring/outbound",
+  },
+  {
+    label: "Transport-Socket",
+    link: "/monitoring/transport-socket",
   }
 ];
 
 export const logsItemsConfig = [
+  {
+    label: "Kafka Topics",
+    link: "/monitoring/logs/kafka-topics",
+  },
   {
     label: "UCI-API",
     link: "/monitoring/logs/uci-api",
@@ -53,5 +65,9 @@ export const logsItemsConfig = [
   {
     label: "Outbound",
     link: "/monitoring/logs/outbound",
+  },
+  {
+    label: "Transport-Socket",
+    link: "/monitoring/logs/transport-socket",
   }
 ];
