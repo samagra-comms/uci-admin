e11c51cca5701cddca254c4f992e7fe609ef2632
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2024-04-08 14:37:16 +0530

    fix: update fail for buckets name change

--- src/api/api-util-functions.ts
+++ src/api/api-util-functions.ts
@@ -315,9 +315,11 @@ export const onBotUpdate = () => {
 
   const reqObj = {
     ...store?.editState,
-    meta: { isPinned: store?.editState?.isPinned },
+    meta: { isPinned: store?.editState?.isPinned ?? false },
+    tags: store?.editState?.tags ? store?.editState?.tags?.split(",") :null,
     id: store.state.id,
   };
+  
 
 
   if (reqObj.startDate) {
